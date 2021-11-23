8052afd23c1bc0bedee14d3c41f5a91a2bef5014
andrepc <andrepc@andrepc.lan>
2021-11-23 23:33:52 +0100

    Initial code

---
+++ script/cpuusage.py
@@ -0,0 +1,22 @@
+import psutil
+import matplotlib.pyplot as plt
+import time
+
+timeusage=[]
+n=0
+usage=[]
+x=0
+t=0
+ram=[]
+while n==0:
+    usage.append(psutil.cpu_percent(1))
+    timeusage.append(t)
+    x=x+1
+    t=t+1
+    plt.plot(timeusage,usage)
+    plt.ylabel('CPU usage')
+    plt.xlabel('Time')
+    plt.savefig('graph.png')
+    time.sleep(3)
+
+
---
+++ script/ramusage.py
@@ -0,0 +1,21 @@
+import psutil
+import matplotlib.pyplot as plt
+import time
+timeusage=[]
+n=0
+t=0
+usage=[]
+x=0
+ram=[]
+while n==0:
+    usage.append(psutil.virtual_memory()[2])
+    timeusage.append(t)
+    x=x+1
+    t=t+1
+    plt.plot(timeusage,usage)
+    plt.ylabel('Ram usage')
+    plt.xlabel('Time')
+    plt.savefig('ram.png')
+    time.sleep(3)
+
+
