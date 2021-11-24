ec3acdbd4fcc88db5e0410ec1118b64d2e2c9c91
Andrea Canale <vivanco11.ac@gmail.com>
2021-11-24 16:14:50 +0100

    Update ramusage.py

--- script/ramusage.py
+++ script/ramusage.py
@@ -6,19 +6,31 @@ n=0
 t=0
 usage=[]
 x=0
+p=0
 ram=[]
 while n==0:
     usage.append(psutil.virtual_memory()[2])
     timeusage.append(t)
     x=x+1
     t=t+1
-    if t>20:
-        del timeusage[0]
-        del usage[0]
-    plt.plot(timeusage,usage)
-    plt.ylabel('Ram usage')
-    plt.xlabel('Time')
-    plt.savefig('ram.png')
-    time.sleep(3)
+    p=p+1
+    if p>150:
+        p=0
+        usage.clear()
+        timeusage.clear()
+        plt.clf()
+        plt.title("RAM Usage")
+        plt.plot(timeusage,usage)
+        plt.ylabel('RAM usage%')
+        plt.xlabel('Time')
+        plt.savefig('ram.png')
+        time.sleep(3)
+    else:
+        plt.title("RAM Usage")
+        plt.plot(timeusage,usage)
+        plt.ylabel('RAM usage%')
+        plt.xlabel('Time')
+        plt.savefig('ram.png')
+        time.sleep(3)
 
 
