1e0954525ec74a7376df60d9ddc4a82bccab078f
Andrea Canale <vivanco11.ac@gmail.com>
2021-11-24 16:14:37 +0100

    Update cpuusage.py

--- script/cpuusage.py
+++ script/cpuusage.py
@@ -7,19 +7,31 @@ n=0
 usage=[]
 x=0
 t=0
+p=0
 ram=[]
 while n==0:
     usage.append(psutil.cpu_percent(1))
     timeusage.append(t)
     x=x+1
     t=t+1
-    if t>20:
-        del timeusage[0]
-        del usage[0]
-    plt.plot(timeusage,usage)
-    plt.ylabel('CPU usage')
-    plt.xlabel('Time')
-    plt.savefig('graph.png')
-    time.sleep(3)
+    p=p+1
+    if p>150:
+        usage.clear()
+        timeusage.clear()
+        p=0
+        plt.clf()
+        plt.title("Cpu Usage")
+        plt.plot(timeusage,usage)
+        plt.ylabel('CPU usage')
+        plt.xlabel('Time')
+        plt.savefig('graph.png')
+        time.sleep(3)
+    else:
+        plt.title("Cpu Usage")
+        plt.plot(timeusage,usage)
+        plt.ylabel('CPU usage')
+        plt.xlabel('Time')
+        plt.savefig('graph.png')
+        time.sleep(3)
 
 
