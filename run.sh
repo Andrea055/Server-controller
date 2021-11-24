dc4a6c88a5e44ec4e9f4dbab03dfa7c5cd974330
Andrea Canale <vivanco11.ac@gmail.com>
2021-11-24 23:11:30 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 sudo apt install nodejs npm
 cd webssh2
 cd app
+cp config.json.sample config.json
+sudo nano config.json
 npm install --production & npm start & ./cpuusage & ./ramusage & sudo node log.js 
 
