6ec1a12d5857a6c2a033c857cfc9b5605ba0e557
Andrea Canale <vivanco11.ac@gmail.com>
2021-11-27 11:10:33 +0100

    fix error and add check for requirements

--- build.sh
+++ build.sh
@@ -1,10 +1,23 @@
 git clone https://github.com/billchurch/webssh2.git
+pip install psutil matplotlib pyinstaller
+python3 -m PyInstaller -F $(pwd)/script/cpuusage.py
+python3 -m PyInstaller -F $(pwd)/script/ramusage.py
+curl -q0- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+nvm install 16.13.0
+npm install -g pkg
+cd $(pwd)/script/
+npm install node-os-utils
+pkg log.js
+pkg sysinfo.js
+cd ..
 cd webssh2
 cd app
 npm install --production
 cd ..
 cd ..
-cd script
-pyinstaller -F cpuusage.py
-pyinstaller -F ramusage.py
- 
+cp $(pwd)/script/dist/cpuusage $(pwd)/webssh2/app
+cp $(pwd)/script/dist/ramusage $(pwd)/webssh2/app
+cp $(pwd)/script/log.js $(pwd)/webssh2/app
+cp $(pwd)/script/sysinfo.js $(pwd)/webssh2/app
+
+
