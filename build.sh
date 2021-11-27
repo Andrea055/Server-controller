c67a8f2ea7bad9d78505bbdc311a653b96f2805a
Andrea Canale <vivanco11.ac@gmail.com>
2021-11-27 11:22:54 +0100

    fix

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
+npm install node-os-utils
 git clone https://github.com/billchurch/webssh2.git
 pip install psutil matplotlib pyinstaller
 python3 -m PyInstaller -F $(pwd)/script/cpuusage.py
 python3 -m PyInstaller -F $(pwd)/script/ramusage.py
-curl -q0- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-nvm install 16.13.0
 npm install -g pkg
 cd $(pwd)/script/
 npm install node-os-utils
 pkg log.js
 pkg sysinfo.js
+cp log-linux $(pwd)/..
+cp sysinfo-linux $(pwd)/..
 cd ..
 cd webssh2
 cd app
@@ -17,7 +18,6 @@ cd ..
 cd ..
 cp dist/cpuusage $(pwd)/webssh2/app
 cp dist/ramusage $(pwd)/webssh2/app
-cp log-linux $(pwd)/webssh2/app
-cp sysinfo-linux $(pwd)/webssh2/app
+
 
 
